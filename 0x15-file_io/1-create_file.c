9e60991e5fe7208c8614f6b90e290e7d915967dc
Fahd Moustakim <cena-fan@live.fr>
2023-10-02 17:50:58 -0700

    pls

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: text to write
  * 
  * Return: If the function fails - -1
- *         Otherwise - 1
+ *      Otherwise - 1
  */
 int create_file(const char *filename, char *text_content)
 {
